ebbd01526a2632fedb443d5a4f56e42e77d2f16b
rodahub <roda.hub17@gmail.com>
2026-03-16 20:46:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
 client.once('ready', () => {
   console.log('RØDA BOT ONLINE');
+});
+
+client.on('messageCreate', message => {
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong! Il bot funziona.");
+  }
 });
 
 client.login(process.env.TOKEN);
