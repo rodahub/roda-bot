280de01219455c3523d728375395e3d258eb6892
rodahub <roda.hub17@gmail.com>
2026-03-16 20:36:16 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log('RØDA BOT ONLINE');
+});
+
+client.login(process.env.TOKEN);
